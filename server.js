5a0b3e15726a7675bb3cd874b432d56ecb4724d5
Vinh Le <vinh@vertics.co>
2020-05-05 15:21:12 +0300

    feat: add server listener

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+if(process.env.NODE_ENV === 'production') {
+	app.get('/*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'))
+	})
+}
+
+app.listen(8081, () => {
+	console.log('App is listening')
+})

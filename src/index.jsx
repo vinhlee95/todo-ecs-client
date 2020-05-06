bdb7f58a9fa3369cd9c45353a42d9761e2f8d76a
Vinh Le <vinh@vertics.co>
2020-05-06 17:15:50 +0300

    refactor component

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,26 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
+  }
   render() {
     return (
       <div>
         <h1>Hello {this.props.name}</h1>
+        <label htmlFor='name-field'>Your name</label>
+        <br />
+        <input
+          id='name-field'
+          type='text'
+          onChange={e => this.setState({name: e.target.value})}
+          />
+        <button onClick={() => alert('Good job!' + this.state.name)}>CLICK ME!!!</button>
+        <br />
         <span>@Created by Vinh Le in 2020</span>
-        <button onClick={() => alert('Good job!')}>CLICK ME!!!</button>
       </div>
     );
   }

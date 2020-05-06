9a5817b729b5ae7a4704a0fff5f2be4ebf19bf52
Vinh Le <vinh@vertics.co>
2020-05-06 11:29:21 +0300

    feat: add some content

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom"
 
 class App extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+      <div>
+        <h1>Hello {this.props.name}</h1>
+        <span>@Created by Vinh Le in 2020</span>
+      </div>
+    );
   }
 }
 
